cd2d4df4e73c8dc2d513485b1bb26cce776efbcf
jansonkwo <jansonkwo@163.com>
2019-07-24 09:14:18 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Docker Images Hello World!");
+    response.end("Hello World for Docker Images Commit!");
 });
 
 const port = process.env.PORT || 1337;
